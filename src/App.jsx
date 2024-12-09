f2d534dbd11fda4d698fae7d418e482e6864f77f
asadbekumarov <asadbekumarov922@gmail.com>
2024-12-09 13:03:02 +0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Header from "./components/header/Header";
-
+import Header from "./components/Header/Header";
 import Hero from "./components/hero/Hero";
 import Partner from "./components/partner/Partner";
 import Result from "./components/result/Result";
